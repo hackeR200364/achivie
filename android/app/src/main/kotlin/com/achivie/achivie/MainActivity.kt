0f19a06afd43c4eaf8e240c3ff7f2281bbb63a92
hackeR200364 <rupamkarmakar1238@gmail.com>
2023-07-25 00:26:16 +0530

    app-size

--- android/app/src/main/kotlin/com/achivie/achivie/MainActivity.kt
+++ android/app/src/main/kotlin/com/achivie/achivie/MainActivity.kt
@@ -1,18 +1,18 @@
 package com.achivie.achivie
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.media.AudioAttributes
-import android.media.AudioFocusRequest
-import android.media.AudioManager
-import android.media.MediaPlayer
-import android.os.Build
-import android.os.Bundle
-import androidx.annotation.RequiresApi
+//import android.annotation.SuppressLint
+//import android.content.Context
+//import android.media.AudioAttributes
+//import android.media.AudioFocusRequest
+//import android.media.AudioManager
+//import android.media.MediaPlayer
+//import android.os.Build
+//import android.os.Bundle
+//import androidx.annotation.RequiresApi
 import io.flutter.embedding.android.FlutterActivity
 //import io.flutter.app.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
+//import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin
 
 class MainActivity: FlutterActivity() {
